04ddee06271051d0bd8716cd1aa62b2c2dc6f83e
Aishah <maishah2540@gmail.com>
2024-12-03 10:11:20 +0200

    update google analytics implementation.

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,11 +2,12 @@ import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import ThemeRegistry from "@/components/ThemeRegistry";
+import Script from "next/script";
 import "./global.css";
 
 export const metadata = {
   title: "Bus Tracker Cyprus",
-  description: "Accesible bus tracking & bus schedule information for TRNC",
+  description: "Accessible bus tracking & bus schedule information for TRNC",
 };
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,6 +16,24 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-CR3KVLL4P3"
+          async
+        />
+        <Script
+          strategy="afterInteractive"
+          id="google-analytics-script"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-CR3KVLL4P3');
+            `,
+          }}
+        />
         <ThemeRegistry>
           <Navbar />
           {children}
